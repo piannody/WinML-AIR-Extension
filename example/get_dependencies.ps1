e6af20a34ce51ef18152f12d36ce9e05fcaec32c
piannody <159562131+piannody@users.noreply.github.com>
2022-12-10 15:55:49 +0000

    Heading fix

--- example/get_dependencies.ps1
+++ example/get_dependencies.ps1
@@ -1,2 +1,3 @@
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::T
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri https://github.com/tuarua/Windows-ML-ANE/releases/download/0.1.0/MLANE.ane?raw=true -O
