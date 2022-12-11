a6748256ca6b7f4bab62a37b999138483d3fd7a4
piannody <159562131+piannody@users.noreply.github.com>
2022-12-11 17:09:06 +0000

    Create fonts

--- example/get_dependencies.ps1
+++ example/get_dependencies.ps1
@@ -1,3 +1,4 @@
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://github.com/tuarua/Windows-ML-ANE/releases/download/0.1.0/MLANE.ane?raw=true -O
+Invoke-WebRequest -Uri https://github.com/tuarua/Windows-ML-ANE/releases/download/0.1.0/MLANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\MLANE.ane"
+Invoke-WebRequest -Uri https://github.com/tuarua/FreSharp/r
