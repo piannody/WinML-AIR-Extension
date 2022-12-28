6cbc5ce3fa393a8fcb3687cf6b8cbcb47a9b7650
piannody <159562131+piannody@users.noreply.github.com>
2022-12-28 13:16:33 +0000

    Fixed tabbing

--- native_extension/ane/build-windows.bat
+++ native_extension/ane/build-windows.bat
@@ -1,3 +1,5 @@
 REM Get the path to the script and trim to get the directory.
 @echo off
-SET SZIP="C:\Program Fi
+SET SZIP="C:\Program Files\7-Zip\7z.exe"
+SET AIR_PATH="D:\dev\sdks\AIR\AIRSDK_32\bin\"
+echo Setting path to current directory 
