69be035abeae30e97f2384551f66d4182a87e765
piannody <159562131+piannody@users.noreply.github.com>
2022-12-29 08:36:49 +0000

    version1.0

--- native_extension/ane/build-windows.bat
+++ native_extension/ane/build-windows.bat
@@ -2,4 +2,13 @@ REM Get the path to the script and trim to get the directory.
 @echo off
 SET SZIP="C:\Program Files\7-Zip\7z.exe"
 SET AIR_PATH="D:\dev\sdks\AIR\AIRSDK_32\bin\"
-echo Setting path to current directory 
+echo Setting path to current directory to:
+SET pathtome=%~dp0
+echo %pathtome%
+
+SET projectName=MLANE
+
+REM Setup the directory.
+echo Making directories.
+
+IF NOT E
