45022cf95d47871b242d73748a50181b0ae474bf
piannody <159562131+piannody@users.noreply.github.com>
2023-01-07 19:26:03 +0000

    drm/i915: flush cursors harder

--- native_extension/ane/build-windows.bat
+++ native_extension/ane/build-windows.bat
@@ -54,3 +54,4 @@ REM Run the build command.
 echo Building Release.
 call %AIR_PATH%adt.bat -package -target ane %pathtome%%projectName%.ane %pathtome%extension_win.xml -swc %pathtome%%projectName%.swc ^
 -platform Windows-x86 -C %pathtome%platforms\win\x86\release %projectName%.dll %projectName%Lib.dll Newtonsoft.Json.dll library.swf ^
+-platform Windows-x86-64 -C %pathtome%platforms\win\x64\release %projectName%.dll %projectName%Lib.dll 
