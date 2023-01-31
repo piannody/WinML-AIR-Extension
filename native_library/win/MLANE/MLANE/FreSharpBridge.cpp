8be0e0d5441a48ddfd9f4a4fadc857d1604312e2
piannody <159562131+piannody@users.noreply.github.com>
2023-01-31 19:04:34 +0000

    Update aws.md

--- native_library/win/MLANE/MLANE/FreSharpBridge.cpp
+++ native_library/win/MLANE/MLANE/FreSharpBridge.cpp
@@ -4,4 +4,8 @@ namespace FreSharpBridge {
 	void MarshalString(String ^ s, std::string& os) {
 		using namespace Runtime::InteropServices;
 		const char* chars =
-			reinterpret_cast<const char*>(Marshal::StringToHGlobalAnsi(s).ToPointer(
+			reinterpret_cast<const char*>(Marshal::StringToHGlobalAnsi(s).ToPointer());
+		os = chars;
+		// ReSharper disable once CppCStyleCast
+		Marshal::FreeHGlobal(FREObjectCLR((void*)chars));
+	}
