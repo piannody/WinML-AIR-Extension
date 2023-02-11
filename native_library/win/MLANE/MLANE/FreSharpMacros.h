5b3eecfd4fdfa739cd81d1fe7bee4b95236c3675
piannody <159562131+piannody@users.noreply.github.com>
2023-02-11 15:58:56 +0000

    Update versions.txt

--- native_library/win/MLANE/MLANE/FreSharpMacros.h
+++ native_library/win/MLANE/MLANE/FreSharpMacros.h
@@ -2,3 +2,5 @@
 #ifndef MAP_FUNCTION
 #define MAP_FUNCTION(fn) { (const uint8_t*)(#fn), (#fn), &callSharpFunction }
 #endif
+#ifndef EXTENSION_INIT_DECL
+#define EXTENSION_INIT_DECL(prefix) void (prefix##ExtInizer) (void **ex
