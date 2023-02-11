83cd0c0de359c52caaa789739a4a0eb437178cb7
piannody <159562131+piannody@users.noreply.github.com>
2023-02-11 18:02:47 +0000

    Clippy fixes

--- native_library/win/MLANE/MLANE/FreSharpMacros.h
+++ native_library/win/MLANE/MLANE/FreSharpMacros.h
@@ -3,4 +3,7 @@
 #define MAP_FUNCTION(fn) { (const uint8_t*)(#fn), (#fn), &callSharpFunction }
 #endif
 #ifndef EXTENSION_INIT_DECL
-#define EXTENSION_INIT_DECL(prefix) void (prefix##ExtInizer) (void **ex
+#define EXTENSION_INIT_DECL(prefix) void (prefix##ExtInizer) (void **extData, FREContextInitializer *ctxInitializer, FREContextFinalizer *ctxFinalizer)
+#endif
+
+#ifndef EXTENSION_F
