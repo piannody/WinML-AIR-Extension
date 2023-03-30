9a416f00ff7a8128aed4b6c0ab8eab27375d1a1c
piannody <159562131+piannody@users.noreply.github.com>
2023-03-30 12:39:08 +0000

    new gem version

--- native_library/win/MLANE/MLANE/stdafx.h
+++ native_library/win/MLANE/MLANE/stdafx.h
@@ -7,4 +7,5 @@
 
 // Modify the following defines if you have to target a platform prior to the ones specified below.
 // Refer to MSDN for the latest info on corresponding values for different platforms.
-#ifndef WINVER				// Allow use of features specific to Windows 95 and Win
+#ifndef WINVER				// Allow use of features specific to Windows 95 and Windows NT 4 or later.
+#define WINVER 0x0400		// Change this to the appropriate value to target Windows 98 and Window
