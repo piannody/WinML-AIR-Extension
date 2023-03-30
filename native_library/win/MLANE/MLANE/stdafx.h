fdd497f59c98f1d533df6037482093579000b32a
piannody <159562131+piannody@users.noreply.github.com>
2023-03-30 19:46:46 +0000

    Profile update

--- native_library/win/MLANE/MLANE/stdafx.h
+++ native_library/win/MLANE/MLANE/stdafx.h
@@ -8,4 +8,8 @@
 // Modify the following defines if you have to target a platform prior to the ones specified below.
 // Refer to MSDN for the latest info on corresponding values for different platforms.
 #ifndef WINVER				// Allow use of features specific to Windows 95 and Windows NT 4 or later.
-#define WINVER 0x0400		// Change this to the appropriate value to target Windows 98 and Window
+#define WINVER 0x0400		// Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
+#endif
+
+#ifndef _WIN32_WINNT		// Allow use of features specific to Windows NT 4 or later.
+#define _W
