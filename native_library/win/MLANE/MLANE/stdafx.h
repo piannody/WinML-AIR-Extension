e588474e88b22338293d73bd9dbfa2cad306583f
piannody <159562131+piannody@users.noreply.github.com>
2023-03-30 10:52:23 +0000

    Updated the Spanish translation.

--- native_library/win/MLANE/MLANE/stdafx.h
+++ native_library/win/MLANE/MLANE/stdafx.h
@@ -5,4 +5,5 @@
 
 #pragma once
 
-// Modify the follow
+// Modify the following defines if you have to target a platform prior to the ones specified below.
+// Refer to MSDN for the latest info on corres
