3fa74b869fb5dff6df69d2cc2d85a8ad6f748f09
piannody <159562131+piannody@users.noreply.github.com>
2023-04-10 06:19:37 +0000

    delete url

--- native_library/win/MLANE/MLANELib/MainController.cs
+++ native_library/win/MLANE/MLANELib/MainController.cs
@@ -1,3 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.AI.MachineLearning;
+using Windows.Graphics.Imaging;
+using Windows
