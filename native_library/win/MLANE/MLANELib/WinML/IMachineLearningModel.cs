7d4c0bb56bfe314f322d5ffe25c13268ec722f06
piannody <159562131+piannody@users.noreply.github.com>
2023-05-18 22:39:33 +0000

    adding badges

--- native_library/win/MLANE/MLANELib/WinML/IMachineLearningModel.cs
+++ native_library/win/MLANE/MLANELib/WinML/IMachineLearningModel.cs
@@ -1,2 +1,6 @@
 ﻿using System.Threading.Tasks;
-using Windows.AI.MachineLearni
+using Windows.AI.MachineLearning;
+
+namespace MLANELib.WinML {
+    public interface IMachineLearningModel {
+        LearningModel Lear
