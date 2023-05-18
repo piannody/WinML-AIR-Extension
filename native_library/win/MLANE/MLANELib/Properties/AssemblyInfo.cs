8e29819cca045b690d44647bc8bbb23399631c81
piannody <159562131+piannody@users.noreply.github.com>
2023-05-18 18:15:22 +0000

    add simplecov

--- native_library/win/MLANE/MLANELib/Properties/AssemblyInfo.cs
+++ native_library/win/MLANE/MLANELib/Properties/AssemblyInfo.cs
@@ -29,4 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default 
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: As
