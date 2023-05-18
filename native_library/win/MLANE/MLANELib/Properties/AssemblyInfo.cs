1ab2663ef2df1c9bcfd9c8ef2c775d2d00e0871e
piannody <159562131+piannody@users.noreply.github.com>
2023-05-18 14:09:22 +0000

    Update requirements

--- native_library/win/MLANE/MLANELib/Properties/AssemblyInfo.cs
+++ native_library/win/MLANE/MLANELib/Properties/AssemblyInfo.cs
@@ -20,4 +20,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("71ced144-ed05-4414-9744-e359ac074b0e"
+[assembly: Guid("71ced144-ed05-4414-9744-e359ac074b0e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      M
