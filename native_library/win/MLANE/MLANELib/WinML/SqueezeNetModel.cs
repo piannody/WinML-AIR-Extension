384de6ffe8cd87b6e93f6ee6df6df23d65e508d5
piannody <159562131+piannody@users.noreply.github.com>
2023-05-29 12:36:32 +0000

    Replace tabs with spaces.

--- native_library/win/MLANE/MLANELib/WinML/SqueezeNetModel.cs
+++ native_library/win/MLANE/MLANELib/WinML/SqueezeNetModel.cs
@@ -1,2 +1,8 @@
 ﻿using System;
-using System.Collections.G
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.AI.MachineLearning;
+
+namespace MLANELib.WinML {
+    pu
